4f3098e6f6fc3b356edc2c22edb98b8411740f31
ZachPorterS <95295241+ZachPorterS@users.noreply.github.com>
2021-11-30 08:24:41 -0600

    Update msg_queue_tester.c

--- msg_queue_tester.c
+++ msg_queue_tester.c
@@ -5,45 +5,88 @@
  * multiple sending and receiving threads.
  *
  */
- #include <stdio.h>
- #include <stdlib.h>
- #include "msg_queue.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "msg_queue.h"
 
  /* Structure to hold thread arguments */
- struct thread_args {
-   int t_id;
-   int num_msgs;
-   struct msg_queue *mq;
+struct thread_args {
+	int t_id;
+	int num_msgs;
+	struct msg_queue *mq;
 
- };
+};
 
- /* Function prototypes */
- void thread_args_init(struct thread_args *args, int t_id, int num_msgs,
- 		      struct msg_queue *mq);
- void * send_msgs(void *args);
- void * receive_msgs(void *args);
+/* Function prototypes */
+void thread_args_init(struct thread_args *args, int t_id, int num_msgs,
+	struct msg_queue *mq);
+void * send_msgs(void *args);
+void * receive_msgs(void *args);
 
- /* Main function */
- int main(int argc, char *argv[]) {
+/* Main function */
+int main(int argc, char *argv[]) {
 
- }
+	pthread_t *sender;
+	pthread_t *receiver;
 
- /* Initialize thread_args structure */
- void thread_args_init(struct thread_args *args, int t_id, int num_msgs,
- 		      struct msg_queue *mq) {
+	struct msg_queue *new_mq;
+	mq_init(new_mq);
 
- }
+	struct thread_args *new_args;
+	thread_args_init(new_args, 2, argc, new_mq);
 
- /* Function for sending thread to execute */
- void * send_msgs(void *args) {
-     //struct thread_args * real_args = (struct thread_args *) args;
-     //struct mq_msg messages;
-     void *p = &thread_args;
+	int i;
+	for (i = 0; i < argc; i++) {
+		pthread_create(sender, NULL, send_msgs, &new_args);
+		pthread_create(receiver, NULL, receive_msgs, &new_args);
+		pthread_join(*sender, NULL);
+		pthread_join(*receiver, NULL);
+	}
 
-     return NULL;
- }
+	return 0;
+}
 
- /* Function for receiving thread to execute */
- void * receive_msgs(void *args) {
-     return NULL;
- }
+/* Initialize thread_args structure */
+void thread_args_init(struct thread_args *args, int t_id, int num_msgs,
+	struct msg_queue *mq) {
+
+	// Initialize Variables
+	struct thread_args *ptr = args;
+	ptr->t_id = t_id;
+	ptr->num_msgs = num_msgs;
+	ptr->mq = mq;
+
+}
+
+/* Function for sending thread to execute */
+void * send_msgs(void *args) {
+	struct thread_args *real_args = (struct thread_args *) args;
+
+	int i;
+	for (i = 0; i < real_args->num_msgs; i++) {
+		struct mq_msg *msg_sent;
+		msg_sent->t_id = i;
+		msg_sent->m_id = i;
+		mq_send(real_args->mq, msg_sent);
+	}
+
+	return NULL;
+}
+
+/* Function for receiving thread to execute */
+void * receive_msgs(void *args) {
+	struct thread_args *real_args = (struct thread_args *) args;
+
+	int i;
+	for (i = 0; i < real_args->num_msgs; i++) {
+		struct mq_msg *msg_receive;
+		msg_receive->t_id = i;
+		msg_receive->m_id = i;
+		mq_receive(real_args->mq, msg_receive);
+		printf("[Sender: ", real_args->mq->messages->t_id);
+		printf(", receiver: %d", msg_receive->t_id, ", msg_id: ", msg_receive->m_id, "]");
+	}
+
+	return NULL;
+}
